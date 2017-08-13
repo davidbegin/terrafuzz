2afb158036a41e4bc4224a91b22ee6596aaa263c
davidbegin <davidmichaelbe@gmail.com>
2017-08-12 17:21:42 -0700

    Vastly improve the terrafuzz script

--- terrafuzz.sh
+++ terrafuzz.sh
@@ -1,11 +1,54 @@
 #!/bin/bash
 
-echo "It's TerraFuzz Time"
+function deploy {
+  cd ..
+  npm run build
+  cd terrafuzz
+  ./populate_bucket.sh
+}
 
-terraform init
+function destroy {
+  ./empty_bucket.sh
+  terraform destroy -var-file=variables.tfvars
+}
 
-# ./terrafuzz deploy
-terraform apply -var-file=variables.tfvars
+function setup_terraform {
+  terraform init
+  terraform get
+}
 
-# ./terrafuzz destroy
-# terraform destroy -var-file=variables.tfvars
+function apply_terraform_changes {
+  terraform apply -var-file=variables.tfvars
+}
+
+function greeting {
+  echo "It's Terrafuzz Time"
+}
+
+function goodbye {
+  echo "You've been Terrafuzzed"
+}
+
+case "$1" in
+        apply)
+            greeting
+            apply_terraform_changes
+            deploy
+            goodbye
+            ;;
+         
+        deploy)
+            greeting
+            deploy
+            goodbye
+            ;;
+         
+        destroy)
+            greeting
+            destroy
+            goodbye
+            ;;
+        *)
+            echo $"Usage: $0 {apply|deploy|destroy|}"
+            exit 1
+esac
